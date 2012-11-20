de4f80e659f6f51883ebd1dcb10fa777db182098
Victor A. Villa Vila <vvilla2099@gmail.com>
2012-11-20 08:08:40 -0500

    ProspectoTest

--- BenedictoXVI/test/ProspectoTest.java
+++ BenedictoXVI/test/ProspectoTest.java
@@ -18,6 +18,22 @@ public class ProspectoTest {
     
     public ProspectoTest() {
     }
+
+    @org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
     
     @BeforeClass
     public static void setUpClass() {
