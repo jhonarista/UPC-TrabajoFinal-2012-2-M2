880722d0ccf3826c7d53dcc001d11d1ef029fdbc
c <daalsuol@gmail.com>
2012-11-22 20:42:38 -0500

    Temp

--- BenedictoXVI/test/ProspectoTest.java
+++ BenedictoXVI/test/ProspectoTest.java
@@ -1,20 +1,47 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
+import Entity.Prospecto;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-/**
- *
- * @author win7
- */
+
 public class ProspectoTest {
     
     public ProspectoTest() {
+    }
+    
+    @Test
+    public void CrearNuevoProspecto(){
+        String prospectoA [] = {"Pedro", "Diaz", "Perez", "pdiaz@hotmail.com", "12345678", "9876543", "01012012"};
+        Prospecto miProspecto = new Prospecto();
+        
+        //assertArrayEquals(prospectoA, miProspecto.crearProspecto("Pedro", "Diaz", "Perez", "pdiaz@hotmail.com", "12345678", "9876543", "01012012"));
+        assertArrayEquals(prospectoA, miProspecto.crearProspecto("Pedro", "Diaz", "Perez", "pdiaz@hotmail.com", "12345678", "9876543", "01012012"));
+        
+    }
+    
+    @Test
+    public void CompruebaSiExisteProspesctoPorDNI(){
+        String prospectoA [] = {"Pedro", "Diaz", "Perez", "pdiaz@hotmail.com", "12345678", "9876543", "01012012"};
+        Prospecto miProspecto = new Prospecto();
+        
+        miProspecto.crearProspecto("Pedro", "Diaz", "Perez", "pdiaz@hotmail.com", "12345678", "9876543", "01012012");
+
+        assertEquals(prospectoA[4], "12345678");
+    }
+    
+    @Test
+    public void EliminaElContenidoDeLosDatosDeUnProspecto(){
+        String prospectoA [] = {"", "", "", "", "", "", ""};
+        Prospecto miProspecto = new Prospecto();
+        
+        //miProspecto.borrarDatos("12345678");
+        
+        assertArrayEquals(prospectoA, miProspecto.borrarDatos("11223344"));
+        
     }
     
     @BeforeClass
@@ -32,9 +59,5 @@ public class ProspectoTest {
     @After
     public void tearDown() {
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+
 }
