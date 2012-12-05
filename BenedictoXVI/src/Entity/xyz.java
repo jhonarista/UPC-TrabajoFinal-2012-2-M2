68efd5364b3a770a1777436a6715c38b4badb238
Victor A. Villa Vila <vvilla2099@gmail.com>
2012-12-05 01:17:15 -0500

    prueba02

---
+++ BenedictoXVI/src/Entity/xyz.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author win7
+ */
+public class xyz {
+    
+}
